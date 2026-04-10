f9defdfd67e5afc341155fd6424e299a149a3663
Aditya kumbhar <aditya.kumbhar@ashidaelectronics.in>
2026-04-10 12:26:49 +0530

    branch_1 first commit

--- main_branch.c
+++ main_branch.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello aditya welcome to git hub");
+    //now this code for branch_1 code
+    int a=10,b=20;
+    int sum=a+b;//this is the code for branch_1
+    printf("hello you are in branch_1\n");
+    printf("Sum of a and b is: %d", sum);
     return 0;
 }
 //this is the version V1.1a
