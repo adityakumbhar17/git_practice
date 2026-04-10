524e24477a1003e715d13f1b79aedd512fccfdf7
Aditya kumbhar <aditya.kumbhar@ashidaelectronics.in>
2026-04-10 14:49:57 +0530

    this is branch 2 v1.2a

--- main_branch.c
+++ main_branch.c
@@ -6,6 +6,16 @@ int main()
     int sum=a+b;//this is the code for branch_1
     printf("hello you are in branch_1\n");
     printf("Sum of a and b is: %d", sum);
+    //now this code for branch_2 code
+    //now we are using that sum  and  chek that number is even or odd
+    if(sum%2==0)
+    {
+        printf("\nSum is even");
+    }
+    else
+    {
+        printf("\nSum is odd");
+    }
     return 0;
 }
 //this is the version V1.1a
