a2daec39f1f3f8e82c57814df98889ac7ebbe6a5
Aditya kumbhar <aditya.kumbhar@ashidaelectronics.in>
2026-04-11 11:56:58 +0530

    this is the commit for conflict in branch 2

--- main_branch.c
+++ main_branch.c
@@ -2,10 +2,10 @@
 int main()
 {
     //now this code for branch_1 code
-    int a=10,b=50;
-    int sum=a+b;//this is the merged  code from branch_1.
+    int a=10,b=500;
+    int sum=a+b;//this is the code for branch_1
     printf("hello you are in branch_1\n");
     printf("Sum of a and b is: %d", sum);
     return 0;
 }
-//this is the version V1.1a //this is the version V1.1B
+//this is the version V1.1a  //this is the version V1.1b  
