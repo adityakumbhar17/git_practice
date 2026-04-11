ca0fb8fbf78cb95c5ecd1cbf0003de5232fc9994
Aditya kumbhar <aditya.kumbhar@ashidaelectronics.in>
2026-04-11 11:21:15 +0530

    this change to chek the git conflict error

--- main_branch.c
+++ main_branch.c
@@ -2,8 +2,8 @@
 int main()
 {
     //now this code for branch_1 code
-    int a=10,b=20;
-    int sum=a+b;//this is the code for branch_1
+    int a=10,b=50;
+    int sum=a+b;//this is the merged  code from branch_1.
     printf("hello you are in branch_1\n");
     printf("Sum of a and b is: %d", sum);
     return 0;
